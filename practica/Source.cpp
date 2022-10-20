27f4744ffe2a80faa747294a00445c45dfd3fdec
ucxxzh@gmail.com <107695960+Arman2517@users.noreply.github.com>
2022-10-20 18:22:59 +0300

    Разработана структура функции Input()

--- practica/Source.cpp
+++ practica/Source.cpp
@@ -60,6 +60,30 @@ void Draw() {
 	cout << "Score: " << score << endl;
 }
 
+void Input() {
+	if (_kbhit()) {
+		switch (_getch())
+		{
+		case 'a':
+			dir = LEFT;
+			break;
+		case 'd':
+			dir = RIGHT;
+			break;
+		case 'w':
+			dir = UP;
+			break;
+		case 's':
+			dir = DOWN;
+			break;
+		case 'x':
+			gameOver = true;
+			break;
+		}
+	}
+
+}
+
 int main() {
 	Setup();
 	while (!gameOver) {
